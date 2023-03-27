88dee58b6509320c470d6b2119779c3abd7e12a1
Lygia Ferronato Bueno <lybueno@gmail.com>
2023-03-26 22:14:38 -0300

    0: Classe Conta criada

---
+++ conta.py
@@ -0,0 +1,44 @@
+from abc import ABC, abstractmethod
+
+class Conta(ABC):
+
+    def __init__(self, account_id: int, balance: float):
+        self.__account_id: int = account_id
+        self._balance: float = balance
+
+
+    def is_valid(self, value: float) -> bool:
+        return value >= 0.0
+    
+
+    @property
+    def account_id(self):
+        return self.__account_id
+    
+
+    @property
+    def balance(self):
+        return self._balance
+    
+
+    @balance.setter
+    def balance(self, new_balance: float):
+        if(self.is_valid(new_balance)):
+            self._balance = new_balance
+        else:
+            raise ValueError("New balance cannot be negative")
+
+
+    @abstractmethod
+    def deposit(self, amount: float):
+        pass
+
+
+    @abstractmethod
+    def withdraw(self, amount: float):
+        pass
+
+"""
+    A Classe Conta é abstrata, pois para a regra de negócio não faz sentido instanciar uma conta e sim conta corrente e poupança
+    Não foi implementado o setter do atributo account_id pois não é comendado deixar código cliente alterar o ID de um objeto
+"""
