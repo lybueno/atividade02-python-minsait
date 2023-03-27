ed2154c879a402fdb12837e5a08d3832f6d227bd
Lygia Ferronato Bueno <lybueno@gmail.com>
2023-03-26 22:21:34 -0300

    3: Classe ExibeMenu

---
+++ menu_periodo.py
@@ -0,0 +1,11 @@
+
+class ExibeMenu():
+
+    def exibir(self) -> str:
+        return input("[s] = segundos"
+              "\n[m] = minutos"
+              "\n[h] = horas"
+              "\n[D] = dias"
+              "\n[M] = meses"
+              "\n[A] = anos"
+              "\nDigite o tempo que deseja calcular o rendimento do valor na poupança (Ex: 11A): ")
